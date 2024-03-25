c956efb96fa7b9166c2405110ffe0c1b43ed5c0b
ThangNguyen <101063068+thangdevalone@users.noreply.github.com>
2024-03-25 18:29:25 +0700

    Update views.py

--- timesheet/views.py
+++ timesheet/views.py
@@ -289,7 +289,7 @@ def check_out(request):
     current_date = timezone.now().date()
 
     existing_timesheet = get_existing_timesheet(emp_id, current_date)
-    timein=existing_timesheet.TimeIn
+    timein = existing_timesheet.TimeIn + timedelta(hours=7)
     if timein.hour < 8 or (timein.hour == 8 and timein.minute < 15):
             timein = timein.replace(hour=8, minute=0, second=0)
     if timein.hour >= 12 and (timein.hour < 14):
