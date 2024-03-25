05255ba3cc806dfeb53964182701e42883cca586
ThangNguyen <101063068+thangdevalone@users.noreply.github.com>
2024-03-25 18:17:38 +0700

    Update views.py

--- timesheet/views.py
+++ timesheet/views.py
@@ -324,7 +324,7 @@ def check_out(request):
     existing_timesheet.save() 
     print(timein, timeout)
     serializer = TimeSheetSerializer(existing_timesheet)
-    serializer.WorkHour= round(work_hours, 2)
+    serializer['WorkHour']= round(work_hours, 2)
     print(serializer.data)
     return Response({"message": "Checked out successfully", "data": serializer.data, "status": status.HTTP_200_OK})
 
