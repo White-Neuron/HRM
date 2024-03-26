0bed6038aca2ae1712876493bf1c75172fab9ae7
ThangNguyen <101063068+thangdevalone@users.noreply.github.com>
2024-03-26 14:40:26 +0700

    Update views.py

--- timesheet/views.py
+++ timesheet/views.py
@@ -317,7 +317,7 @@ def check_out(request):
     if timeout.hour >= 12 and (timeout.hour < 14 ):
         timeout = timeout.replace(hour=12, minute=00, second=0)
     if timein.time() < time(12, 0) and timeout.time() > time(14,0):
-        work_hours =(timeout+7 - timein).total_seconds() / 3600- 2 
+        work_hours =(timeout - timein).total_seconds() / 3600- 2 
         print("a")
     else:
         work_hours =(timeout - timein).total_seconds() / 3600
