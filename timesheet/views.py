077fe52bbf33b87c2aa1efea95ce564d6148a623
Hoàng Vũ Nghị <hoangvunghi2004@gmail.com>
2024-04-17 15:55:14 +0700

    u

--- timesheet/views.py
+++ timesheet/views.py
@@ -695,9 +695,11 @@ def user_timesheet_tasks(request):
         return Response({
             "data": {
                 "EmpName": emp_name,
-                "TimeIn":timein,
-                "TimeOut":None,
-                "Tasks": []
+                "data":{
+                    "TimeIn":timein,
+                    "TimeOut":None,
+                    "Tasks": []
+                }
             },
             "status": status.HTTP_200_OK,
             "message": "No timesheet tasks found."
