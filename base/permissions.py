1d4d6fcb75cb6d8f7c4da0cb4dd7a3287adc6597
Hoàng Vũ Nghị <hoangvunghi2004@gmail.com>
2024-04-20 14:52:36 +0700

    u

--- base/permissions.py
+++ base/permissions.py
@@ -1,8 +1,6 @@
 from rest_framework import permissions
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
         return request.user and request.user.EmpID.RoleID.RoleName == 'CEO'
 
     def has_object_permission(self, request, view, obj):
