312adc6999ed9335c3449949710dc1314adaf016
Nguyễn Tú Anh <ntanhtm@gmail.com>
2024-08-01 21:25:04 +0700

    debug

--- hrmm/settings.py
+++ hrmm/settings.py
@@ -150,13 +150,13 @@ DATABASES = {
     }}
 
 #database sqlite3
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
         
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }}
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 # SESSION_COOKIE_SECURE = True
